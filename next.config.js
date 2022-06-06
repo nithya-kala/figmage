2567f4c03cee2bb891da179218835615a4e66ebd
Nithya <nithyakala1988@gmail.com>
2022-06-07 01:11:14 +0530

    Added basePath to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/figmage",
+  assetPrefix: "/figmage/", // assetPrefix requires the trailing slash
 }
 
 module.exports = nextConfig
