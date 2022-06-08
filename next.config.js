d2135243bf2d2f16700026f8e1c7b6f59518dd5c
Nithya <nithyakala1988@gmail.com>
2022-06-08 23:56:55 +0530

    Set HOST_ENV to ghpages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const isGHPages =
   process.env.HOST_ENV && process.env.HOST_ENV.toLowerCase() === "ghpages"
+    ? "prod"
+    : "local"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: (isGHPages && "/figmage") || "",
-  assetPrefix: (isGHPages && "/figmage/") || "", // assetPrefix requires the trailing slash
+  basePath: isGHPages === "prod" ? "/figmage" : "",
+  assetPrefix: isGHPages === "prod" ? "/figmage" : "", // assetPrefix requires the trailing slash
 }
 
 module.exports = nextConfig
